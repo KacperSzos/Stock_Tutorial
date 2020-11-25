eff42b407c8319c57c3861f3e55f9040088b0c97
KacperSzos <kacper.szostakow@gmail.com>
2020-11-25 01:11:03 +0100

    Add files via upload

---
+++ historical data.py
@@ -0,0 +1,15 @@
+from iexfinance.stocks import Stock
+from iexfinance.stocks import get_historical_data
+import datetime
+import pandas
+
+start = datetime.date(2020, 1, 1)
+end = datetime.date(2020, 3, 1)
+api_key = "pk_1d130ebd9d1943458f3443d55bc44c27"
+
+df = pandas.DataFrame(get_historical_data('AAPL', start, end, token = api_key, close_only=True, output_format='pandas'))
+
+# print(df)
+
+df.to_csv(r'/Users/kszostakow/desktop/test2.csv', index=True)
+
